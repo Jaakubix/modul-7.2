619bd11cade37ae0e4ba4478f92f1622c4f2fa7e
Jakub Piskorz <kpiskorz13@gmail.com>
2021-11-21 19:20:36 +0100

    create virtual enviroment and add code from previous modules to program.py

---
+++ env/program.py
@@ -0,0 +1,14 @@
+class tel:
+    def __init__(self,imie,nazwisko,firma,stanowisko,mail):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.firma = firma
+        self.stanowisko = stanowisko
+        self.mail = mail
+os1 = tel(imie="Janusz", nazwisko="Ozdoba", firma="Filker", stanowisko="kierownik", mail="ozdoba@gmail.com", nrpryw="781698732", nrsluz="884512368")
+os2 = tel(imie="Agata", nazwisko="Prawik", firma="Gustewo", stanowisko="sekretarka", mail="agata1997@wp.pl", nrpryw="981654792", nrsluz="946138729")
+os3 = tel(imie="Tadeusz", nazwisko="Kowalski", firma="Gasper", stanowisko="brygadzista", mail="najlepszybrygadzista@o2.pl", nrpryw="781369584", nrsluz="784293571")
+os4 = tel(imie="Marcin", nazwisko="Prokop", firma="TVN", stanowisko="prezenter", mail="prokop123@gmail.com", nrpryw="781057394", nrsluz="791827634")
+os5 = tel(imie="Hubert", nazwisko="Mikowski", firma="Jebudu", stanowisko="pilot", mail="pilottowarowy@hotmail.com", nrpryw="112684999", nrsluz="781025874")
+os6 = tel(imie="Jadwiga", nazwisko="Stolarczyk", firma="Bislaut", stanowisko="projektantka wnetrz", mail="projektywnetrz@yahoo.com", nrpryw="778451369", nrsluz="305470982")
+wizytowka = [os1,os2,os3,os4,os5,os6]
