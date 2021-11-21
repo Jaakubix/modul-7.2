b85989e5e78ce03194da5b417f2508cae903c328
Jakub Piskorz <kpiskorz13@gmail.com>
2021-11-21 19:59:15 +0100

    fix few issues

--- env/program.py
+++ env/program.py
@@ -1,22 +1,22 @@
 class tel:
-    def __init__(self,imie,nazwisko,firma,stanowisko,mail):
+    def __init__(self,imie,nazwisko,mail):
         self.imie = imie
         self.nazwisko = nazwisko
         self.mail = mail
 class BaseContact(tel):
-    def __init__(self, nrpryw, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, nrpryw, imie, nazwisko, mail):
+        super().__init__(imie, nazwisko, mail)
         self.nrpryw = nrpryw
+    def contact(self):
+        print(f"Wybieram numer +48 {self.nrpryw} i dzwonię do {self.imie} {self.nazwisko}")
 class BusinessContact(tel):
-    def __init__(self, firma, stanowisko, nrsluz, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, firma, stanowisko, nrsluz, imie, nazwisko, mail):
+        super().__init__(imie, nazwisko, mail)
         self.firma = firma
         self.stanowisko = stanowisko
         self.nrsluz = nrsluz
-os1 = tel(imie="Janusz", nazwisko="Ozdoba", firma="Filker", stanowisko="kierownik", mail="ozdoba@gmail.com", nrpryw="781698732", nrsluz="884512368")
-os2 = tel(imie="Agata", nazwisko="Prawik", firma="Gustewo", stanowisko="sekretarka", mail="agata1997@wp.pl", nrpryw="981654792", nrsluz="946138729")
-os3 = tel(imie="Tadeusz", nazwisko="Kowalski", firma="Gasper", stanowisko="brygadzista", mail="najlepszybrygadzista@o2.pl", nrpryw="781369584", nrsluz="784293571")
-os4 = tel(imie="Marcin", nazwisko="Prokop", firma="TVN", stanowisko="prezenter", mail="prokop123@gmail.com", nrpryw="781057394", nrsluz="791827634")
-os5 = tel(imie="Hubert", nazwisko="Mikowski", firma="Jebudu", stanowisko="pilot", mail="pilottowarowy@hotmail.com", nrpryw="112684999", nrsluz="781025874")
-os6 = tel(imie="Jadwiga", nazwisko="Stolarczyk", firma="Bislaut", stanowisko="projektantka wnetrz", mail="projektywnetrz@yahoo.com", nrpryw="778451369", nrsluz="305470982")
-wizytowka = [os1,os2,os3,os4,os5,os6]
+    def contact(self):
+        print(f"Wybieram numer +48{self.nrsluz} i dzwonię do {self.imie} {self.nazwisko}")
+
+os1 = BaseContact(imie="Janusz", nazwisko="Ozdoba", mail="ozdoba@gmail.com", nrpryw="781698732")
+os1_biz = BusinessContact(imie="Janusz", nazwisko="Ozdoba", mail="ozdoba@gmail.com", firma="Filker", stanowisko="kierownik", nrsluz="884512368")
