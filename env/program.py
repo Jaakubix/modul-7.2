ecd635fa8b7e957765b194e3a2bfc435a776c8fa
Jakub Piskorz <kpiskorz13@gmail.com>
2021-11-22 15:23:53 +0100

    add name and lastname length counter

--- env/program.py
+++ env/program.py
@@ -1,8 +1,18 @@
+from faker import Faker
+fake_data = Faker()
 class tel:
     def __init__(self,imie,nazwisko,mail):
         self.imie = imie
         self.nazwisko = nazwisko
         self.mail = mail
+        
+        self.length = 0
+        @property
+        def label_length(self):
+            return self.length
+        @label_length.setter
+        def label_length(self)
+            self.length = len(imie) + len(nazwisko)
 class BaseContact(tel):
     def __init__(self, nrpryw, imie, nazwisko, mail):
         super().__init__(imie, nazwisko, mail)
@@ -18,5 +28,5 @@ class BusinessContact(tel):
     def contact(self):
         print(f"Wybieram numer +48{self.nrsluz} i dzwonię do {self.imie} {self.nazwisko}")
 
-os1 = BaseContact(imie="Janusz", nazwisko="Ozdoba", mail="ozdoba@gmail.com", nrpryw="781698732")
-os1_biz = BusinessContact(imie="Janusz", nazwisko="Ozdoba", mail="ozdoba@gmail.com", firma="Filker", stanowisko="kierownik", nrsluz="884512368")
+os1 = BaseContact(imie="Janusz", nazwisko="Ozdoba", mail="ozdoba@gmail.com", nrpryw="781698732") #dane testowe
+os1_biz = BusinessContact(imie="Janusz", nazwisko="Ozdoba", mail="ozdoba@gmail.com", firma="Filker", stanowisko="kierownik", nrsluz="884512368") #dane testowe
